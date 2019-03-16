15ff7fcacdfd1b93996aa27017e868ccee0467a4
Andres <aguirregzz97@gmail.com>
2019-03-15 22:26:23 -0600

    finished

--- getWeatherDaraWithCallbacks/code.js
+++ getWeatherDaraWithCallbacks/code.js
@@ -8,36 +8,66 @@ const readline = require('readline').createInterface({
 
 readline.question('Ingresa el nombre de la ciudad: ', function(city){
 		urlMapBox = `https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.json?access_token=pk.eyJ1IjoiYWd1aXJyZWd6ejk3IiwiYSI6ImNqdDNpNW1iYzJpYmk0M29kODlvbDlyb2gifQ.ADx-gePuSyWoGxiFlNB47w&limit=1`
-		getCoordinates(city)
+		getCoordinates(city, (error) => {
+			console.log(error)
+		})
 		readline.close()
 	}
 )
 
-getWeatherInfo = (long, lat) => {
+getWeatherInfo = (long, lat, callback) => {
 	let urlDarkSky = `https://api.darksky.net/forecast/64eb360db2951ebe9307279c07b37280/${lat},${long}?units=si&lang=es`
 	request.get({ url: urlDarkSky, json: true }, function(error, response, body) {
-			if(error){
-				console.log(error)
-			}
-			let temp = body.currently.temperature
-			let precipitationProb = body.currently.precipProbability
-			let day = body.currently.summary
-			let dayResponse = `${day}. Actualmente esta a ${temp} °C. Hay ${precipitationProb}% de probabilidad de lluvia.`
-			console.log(dayResponse)
+		if (error) {
+			callback('Hubo error en el weatherInfo')
+			return
+		}
+
+		if (body === undefined) {
+			callback('No tienes internet prro')
+			return
+		}
+
+		if (response.statusCode !== 200) {
+			callback('Hubo algun error encontrando la temperatura de estas coordenadas')
+			return
+		}
+
+		let temp = body.currently.temperature
+		let precipitationProb = body.currently.precipProbability
+		let day = body.currently.summary
+		let dayResponse = `${day}. Actualmente esta a ${temp} °C. Hay ${precipitationProb}% de probabilidad de lluvia.`
+		console.log(dayResponse)
 
 	})
 }
 
-getCoordinates = (cityName) => {
+getCoordinates = (cityName, callback) => {
 	request.get({url:urlMapBox, json:true }, function(error,response,body) {
-		
-		if(error || cityName === ""){
-			console.log(error)
-		} else {
-			console.log(body.features[0].place_name)
-			let longitude = body.features[0].center[0]
-			let latitude = body.features[0].center[1]
-			getWeatherInfo(longitude,latitude)
+		if (error) {
+			callback('Hubo error en el mapbox')
+			return
 		}
+
+		if (body === undefined) {
+			callback('No tienes internet prro')
+			return
+		}
+
+		if (response.statusCode !== 200) {
+			callback('El nombre de la ciudad esta vacio')
+			return
+		}
+
+		if (body.features.length === 0) {
+			callback('El nombre de la ciudad no existe')
+			return
+		}
+
+		let longitude = body.features[0].center[0]
+		let latitude = body.features[0].center[1]
+		getWeatherInfo(longitude,latitude,  (err) => {
+			console.log(err)
+		})
 	})
 }
